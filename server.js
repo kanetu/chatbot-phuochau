621505bf85331dac2b0b02e3573b94ab919f9310
kanetu <kanetu731@gmail.com>
2018-09-14 12:43:00 +0700

    new update

--- server.js
+++ server.js
@@ -28,6 +28,11 @@ app.get('/webhook', function(req, res) {
   res.send('Error, wrong validation token');
 });
 
+var arr = [
+  'Giá xe bao nhiêu?',
+  'Tuyến đường xe chạy'
+]
+
 // Xử lý khi có người nhắn tin cho bot
 app.post('/webhook', function(req, res) {
   var entries = req.body.entry;
@@ -35,20 +40,73 @@ app.post('/webhook', function(req, res) {
     var messaging = entry.messaging;
     for (var message of messaging) {
       var senderId = message.sender.id;
+      console.log(message);
       if (message.message) {
         // If user send text
         if (message.message.text) {
-          var text = message.message.text;
+          
           console.log(text); // In tin nhắn người dùng
-          sendMessage(senderId, "Tui là bot đây: " + text);
+          questionMenu(senderId);
         }
+        
       }
+      if(message.postback){
+          var text = message.postback.title;
+          if( text == arr[0]){
+            sendMessage(senderId,"Giá vé Trà Vinh -> TP.HCM: 110k, Giá vé Trà Vinh -> Bình Dương: 150k, Đưa rước tận nơi!");
+          }
+          if( text == arr[1]){
+            sendMessage(senderId,"Xe chạy theo tuyến đường Trà Vinh -> Bến Tre -> TP.HCM -> Bình Dương ");
+          }
+        }
     }
   }
 
   res.status(200).send("OK");
 });
 
+
+// Gửi thông tin tới REST API để trả lời
+function questionMenu(senderId) {
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {
+      access_token: "EAAaiUzKwmRsBABT7awVDCfMrHx46H2rAbtmN0WazUNhQhUyapK7BY7sSDAUTDgIEMltGanPC05HPMv9X4h0gvix3kcZAnA0E0oYAOBlMKWtHzgS7Fvfw0cqBWxY1ZBQWtcgEZBdihEJtsoP2jnXsbbRQo2hgkQf63PisZBBlPgZDZD",
+    },
+    method: 'POST',
+    json: {
+      recipient: {
+        id: senderId
+      },
+      "message":{
+      "attachment":{
+        "type":"template",
+        "payload":{
+          "template_type":"button",
+          "text":"Xin chào, bạn muốn hỏi thông tin gì?",
+          "buttons":[
+            {
+              "type":"postback",
+              "title":"Giá xe bao nhiêu?",
+              "payload":"DEVELOPER_DEFINED_PAYLOAD"
+            },
+            {
+              "type":"postback",
+              "title":"Tuyến đường xe chạy",
+              "payload":"DEVELOPER_DEFINED_PAYLOAD"
+            },
+            {
+            "type":"phone_number",
+            "title":"Nhấn để gọi ngay!",
+            "payload":"+841636037389"
+          }
+          ]
+        }
+      }
+    },
+    }
+  });
+}
 
 // Gửi thông tin tới REST API để trả lời
 function sendMessage(senderId, message) {
