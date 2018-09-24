467a4993569ce8cf8b5fe211d62b5d4d2e5a9243
Sufuijk desktop <sufuijk@gmail.com>
2018-09-25 00:19:01 +0700

    update

--- server.js
+++ server.js
@@ -15,8 +15,15 @@ app.use(bodyParser.urlencoded({
 var server = http.createServer(app);
 var request = require("request");
 
+
 app.get('/', (req, res) => {
   res.send("Home page. Server running okay.");
+});
+
+app.get('/setup',function(req,res){
+
+  getStarted();
+  initMenu();
 });
 
 // Đây là đoạn code để tạo Webhook
@@ -30,18 +37,15 @@ app.get('/webhook', function(req, res) {
 
 var arr = [
   'Giá xe bao nhiêu?',
-  'Tuyến đường xe chạy'
-]
+  'Tuyến đường xe chạy',
+  'Địa chỉ bãi xe tại TP.HCM',
+  'Địa chỉ bãi xe tại Bình Dương',
+  'Xe có nhận gửi đồ hay không?'
+];
 
+var ACCESS_TOKEN = "EAAQEywipfk8BADgBSBWzE1PepzEoqZCau0qUo47PZCcUu9unjFFrMpB9eZCE1z027pZC8KPppQhNrOYd2A5aJWuwIHsJIZC4PKiM3ZBJmd6YhZCFiONFOk8avzZA2iPTwtsT26VwtzTLKZACiN8ZArpDxtY30s3CVznwoNpupg3Kbk3wZDZD";
+var MESSAGE_APP_ID = 263902037430900;
 
-// Yêu cầu: làm một hàm phát hiện key như trong mảng ans[]
-// - Duyệt qua các ele trong ans và chuỗi s có chứa ele
-// trong mảng thì xuất ra 1 câu thông báo 'detect word'
-// - Chức năng giống LIKE trong truy vấn CSDL
-// vd: 
-// 1. "xin cam on" => detect word
-// 2. "cam on moi nguoi" => detect word
-// Gợi ý: sử dụng 
 var ans = [
   'cam on',
   'cảm ơn',
@@ -63,14 +67,14 @@ function detectThanks(s){
 
 
 
-
 // Xử lý khi có người nhắn tin cho bot
 app.post('/webhook', function(req, res) {
-  var entries = req.body.entry;
-  for (var entry of entries) {
-    var messaging = entry.messaging;
-    for (var message of messaging) {
-      var senderId = message.sender.id;
+
+ let entries = req.body.entry;
+  for (let entry of entries) {
+    let messaging = entry.messaging;
+    for (let message of messaging) {
+      let senderId = message.sender.id;
       console.log(message);
       if (message.message) {
         // If user send text
@@ -82,17 +86,39 @@ app.post('/webhook', function(req, res) {
           }
         
         }
+        
       }
-
       if(message.postback){
-          var text = message.postback.title;
-          if( text == arr[0]){
+       
+        if( message.postback ){
+          var text = message.postback.payload;
+          if( text == 'GIA_XE'){
             sendMessage(senderId,"Giá vé Trà Vinh -> TP.HCM: 110k, Giá vé Trà Vinh -> Bình Dương: 150k, Đưa rước tận nơi!");
           }
-          if( text == arr[1]){
+          if( text == 'TUYEN_DUONG'){
             sendMessage(senderId,"Xe chạy theo tuyến đường Trà Vinh -> Bến Tre -> TP.HCM -> Bình Dương ");
           }
-        }
+          if( text == 'BAIXE_TPHCM'){
+            sendMessage(senderId,"Địa chỉ bãi xe du lịch Phước Hậu tại Bình Dương: 249 Đường số 5 Phường Bình Trị Đông B, Quận Bình Tân");
+          }
+          if( text == 'BAIXE_BD'){
+            sendMessage(senderId,"Địa chỉ bãi xe du lịch Phước Hậu tại Bình Dương: Vòng xoay Phú Mỹ, Thủ Dầu 1, Bình Dương");
+          }
+          if( text == 'GUIDO'){
+            sendMessage(senderId,"Xe có nhận gửi đồ, người nhận hàng đến bãi xe nhận đồ khi được phụ xe gọi ra nhận.");
+          }
+          if( text === "CHAT_WITH_ADMIN" )
+          {
+            passThreadControl(senderId);
+          }
+          if( text === "CHAT_WITH_BOT" )
+          {
+            takeThreadControl(senderId);
+          }
+      }
+      }
+
+      
     }
   }
 
@@ -105,7 +131,7 @@ function questionMenu(senderId) {
   request({
     url: 'https://graph.facebook.com/v2.6/me/messages',
     qs: {
-      access_token: "EAAaiUzKwmRsBABT7awVDCfMrHx46H2rAbtmN0WazUNhQhUyapK7BY7sSDAUTDgIEMltGanPC05HPMv9X4h0gvix3kcZAnA0E0oYAOBlMKWtHzgS7Fvfw0cqBWxY1ZBQWtcgEZBdihEJtsoP2jnXsbbRQo2hgkQf63PisZBBlPgZDZD",
+      access_token: ACCESS_TOKEN,
     },
     method: 'POST',
     json: {
@@ -122,17 +148,17 @@ function questionMenu(senderId) {
             {
               "type":"postback",
               "title":"Giá xe bao nhiêu?",
-              "payload":"DEVELOPER_DEFINED_PAYLOAD"
+              "payload":"GIA_XE"
             },
             {
               "type":"postback",
               "title":"Tuyến đường xe chạy",
-              "payload":"DEVELOPER_DEFINED_PAYLOAD"
+              "payload":"TUYEN_DUONG"
             },
             {
-            "type":"phone_number",
-            "title":"Gọi 0985303222",
-            "payload":"+84985303222"
+              "type":"postback",
+              "title":"Nói chuyện trực tiếp với admin",
+              "payload": "CHAT_WITH_ADMIN"
             }
             
             
@@ -149,7 +175,7 @@ function sendMessage(senderId, message) {
   request({
     url: 'https://graph.facebook.com/v2.6/me/messages',
     qs: {
-      access_token: "EAAaiUzKwmRsBABT7awVDCfMrHx46H2rAbtmN0WazUNhQhUyapK7BY7sSDAUTDgIEMltGanPC05HPMv9X4h0gvix3kcZAnA0E0oYAOBlMKWtHzgS7Fvfw0cqBWxY1ZBQWtcgEZBdihEJtsoP2jnXsbbRQo2hgkQf63PisZBBlPgZDZD",
+      access_token: ACCESS_TOKEN,
     },
     method: 'POST',
     json: {
@@ -161,6 +187,170 @@ function sendMessage(senderId, message) {
       },
     }
   });
+}
+
+//getStarted();
+function getStarted(){
+  var messageData = {
+    "get_started":{
+    "payload":"<GET_STARTED_PAYLOAD>"
+  }
+        };
+
+        // Start the request
+        request({
+            url: 'https://graph.facebook.com/v2.6/me/messenger_profile',
+             qs: {
+              access_token: ACCESS_TOKEN,
+            },
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            form: messageData
+        },
+        function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                // Print out the response body
+                console.log(body);
+
+            } else { 
+                
+                console.log(body);
+            }
+        });  
+} 
+//initMenu();
+function initMenu(){
+  var messageData = {
+    "persistent_menu":[
+    {
+      "locale":"default",
+      "composer_input_disabled":false,
+      "call_to_actions":[  
+       {  
+          "type":"nested",
+          "title":"Thông tin về xe",
+          "call_to_actions":[  
+             {  
+                "type":"postback",
+                "title":"Giá xe bao nhiêu?",
+                "payload":"GIA_XE"
+             },
+             {  
+                "type":"postback",
+                "title":"Tuyến đường xe chạy",
+                "payload":"TUYEN_DUONG"
+             }
+          ]
+       },
+       {  
+          "type":"nested",
+          "title":"Nói chuyện với",
+          "call_to_actions":[  
+             {  
+                "type":"postback",
+                "title":"Admin",
+                "payload":"CHAT_WITH_ADMIN"
+             },
+             {  
+                "type":"postback",
+                "title":"Trả lời tự động",
+                "payload":"CHAT_WITH_BOT"
+             }
+          ]
+       },
+       {  
+          "type":"nested",
+          "title":"Địa chỉ bãi xe",
+          "call_to_actions":[  
+             {  
+                "type":"postback",
+                "title":"tại TP.HCM",
+                "payload":"BAIXE_TPHCM"
+             },
+             {  
+                "type":"postback",
+                "title":"tại Bình Dương",
+                "payload":"BAIXE_BD"
+             },
+    
+          ]
+       }
+    ] 
+    }]
+    };
+
+        // Start the request
+    request({
+    uri: 'https://graph.facebook.com/v2.6/me/messenger_profile',
+    qs: { 
+      access_token: ACCESS_TOKEN 
+    },
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    form: messageData
+    },
+    function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        // Print out the response body
+        console.error(body);
+      } else { 
+        console.error(body);
+      }
+    });
+  } 
+
+
+
+
+
+
+function passThreadControl(senderId) {
+    request(
+        {
+            uri: "https://graph.facebook.com/v2.6/me/pass_thread_control",
+            qs: { access_token: ACCESS_TOKEN }, // access token of your app (bot server) to your page
+            method: "POST",
+            json: {
+                recipient: {
+                    id: senderId
+                },
+                target_app_id: MESSAGE_APP_ID // you can retrieve this in your page setting after linking zendesk message - 200646160103180
+            }
+        },
+        (err, res, body) => {
+            if (err || body.error) {
+                console.log("UNABLE TO SEND PASS THREAD REQUEST", err || body.error);
+            } else {
+                sendMessage(senderId,"Bạn đang nói chuyện với admin");
+                console.log("PASSED THREAD TO MESSAGE DASHBOARD BOT");
+            }
+        }
+    );
+}
+
+
+function takeThreadControl(senderId) {
+    request(
+        {
+            uri: "https://graph.facebook.com/v2.6/me/take_thread_control",
+            qs: { access_token: ACCESS_TOKEN }, // access token of your app (bot server) to your page
+            method: "POST",
+            json: {
+                recipient: {
+                    id: senderId
+                },
+                metadata:"String to pass to the secondary receiver" 
+            }
+        },
+        (err, res, body) => {
+            if (err || body.error) {
+                console.log("UNABLE TO SEND PASS THREAD REQUEST", err || body.error);
+            } else {
+                sendMessage(senderId,"Bạn đang nói chuyện với hệ thống trả lời tự động!");
+                console.log("TAKED THREAD TO MESSAGE DASHBOARD BOT");
+            }
+        }
+    );
 }
 
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
